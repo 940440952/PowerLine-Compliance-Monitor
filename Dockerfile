198decf619a1297bcfb161985be5cf4d8cabf0d4
GTM <940449052@qq.com>
2024-07-22 16:10:52 +0800

    sgement dockerfile更新

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,24 @@
-# 使用官方PyTorch镜像作为基础镜像
-FROM pytorch/pytorch:latest
-
-# 设置非交互式安装，避免tzdata等包的配置交互
-ARG DEBIAN_FRONTEND=noninteractive
-
-# 安装系统依赖
-RUN apt-get update && apt-get install -y \
-    libgl1-mesa-glx \
-    libglib2.0-0 \
-    fonts-noto-cjk \
-    && rm -rf /var/lib/apt/lists/*
+# 使用官方的 Python 基础镜像
+FROM python:3.9-slim
 
 # 设置工作目录
 WORKDIR /app
 
-# 安装Flask、Gunicorn、Numpy、Pillow、OpenCV和YOLO
-# 使用清华大学镜像源来加速下载
-RUN pip install -i https://pypi.tuna.tsinghua.edu.cn/simple flask gunicorn numpy Pillow opencv-python ultralytics
+# 复制当前目录的内容到工作目录
+COPY main_run_linux.py wsgi.py requirements.txt ./
+COPY ./Font/ ./Font/
+COPY ./model/ ./model/
 
-# 复制所需文件到容器内的工作目录
-COPY main_run_linux.py yolov8_seg.pt ./
+# 安装必要的依赖项
+RUN apt-get update && apt-get install -y \
+    libgl1-mesa-glx \
+    libglib2.0-0 \
+    && pip install --no-cache-dir -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 
-# 暴露端口
+# 暴露应用程序的端口
 EXPOSE 8888
 
-# 当容器启动时使用Gunicorn运行应用
-CMD ["gunicorn", "--bind", "0.0.0.0:8888", "main_run_linux:app"]
+# 启动命令
+CMD ["gunicorn", "--workers", "3", "--bind", "0.0.0.0:8888", "wsgi:app"]
+
+
--- main_run_linux.py
+++ main_run_linux.py
@@ -1,26 +1,24 @@
 import base64
+import datetime
 
-from flask import Flask, request
 import numpy as np
-from torch import nn
-from torchvision.models import resnet50
+import pytz
 import torch
-from torchvision import transforms
 from PIL import Image, ImageDraw, ImageFont
 from ultralytics import YOLO
 import cv2
-from flask import Flask, jsonify  # flask库
+from flask import Flask, jsonify, request  # flask库
 import time
 from io import BytesIO
 from torchvision.models import resnet50, ResNet50_Weights
+import platform
+
 app = Flask(__name__)
-
-
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # 加载训练好的模型参数
-ymodel = YOLO('yolov8_seg.pt')
+ymodel = YOLO('./model/yolov8_seg.pt')
 
 # 0: sucha_position
 # 1: sucha_slack
@@ -31,19 +29,53 @@ ymodel = YOLO('yolov8_seg.pt')
 # 6: no_violation
 # 7: person
 
-class_names = {0: '速插位置系错', 1: '速插松弛', 2: '速插低挂高用', 3: '速插未系', 4: '安全带未系', 5: '安全带低挂高用', 6: '无违规', 7: '人'}
+class_names = {0: '速插位置系错', 1: '速插松弛', 2: '速插低挂高用', 3: '速插未系', 4: '安全带未系', 5: '安全带低挂高用',
+               6: '无违规', 7: '人'}
 
 
-@app.route('/upload', methods=['POST'])
+def get_font():
+    system = platform.system()
+    if system == "Windows":
+        return "./Font/simhei.ttf"
+    elif system == "Linux":
+        return "./Font/NotoSansSC-Regular.ttf"
+    else:
+        return None
+
+
+@app.route('/v1/service/imageTask', methods=['POST'])
 def upload():
     if request.method == 'POST':
-        file = request.files['image']
-        image = Image.open(file).convert("RGB")
+        data = request.get_json()
+        if 'image' not in data:
+            raise ValueError("No image part")  # 未上传图片或格式错误
+
+        if 'algCode' not in data:
+            raise ValueError("No algCode")  # 无算法编码
+
+        alg_code = data['algCode']
+        if alg_code != "81001001":
+            raise ValueError("algCode value error")  # 算法编码错误
+
+        if 'analyseId' not in data:
+            raise ValueError("No analyseId")  # 无分析id
+
+        image_data = data['image']
+        image_bytes = base64.b64decode(image_data)
+        image_np_arr = np.frombuffer(image_bytes, np.uint8)
+
+        try:
+            image = cv2.imdecode(image_np_arr, cv2.IMREAD_COLOR)
+            if image is None:
+                raise ValueError("Error decoding image")  # 无法读取该格式
+        except Exception as e:
+            raise ValueError(f"Error opening image: {str(e)}")
+
         start_time = time.time()
 
         results = ymodel.predict(image, save=False, show_conf=False)
         detections = []
-
+        violations_list = []
         # OpenCV 处理图像
         cv_image = np.array(image)
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR)
@@ -52,17 +84,25 @@ def upload():
         for r in results:
             bbox_list = r.boxes.xyxy.tolist()  # 获取边界框列表
             predict_res = r.boxes.cls.cpu().numpy()  # 获取类别预测结果
+            conf_res = r.boxes.conf.tolist()
 
             for i, bbox in enumerate(bbox_list):
                 x1, y1, x2, y2 = [int(coord) for coord in bbox]
                 object_class_id = int(predict_res[i])  # 获取对象的类别ID
                 object_class_name = class_names.get(object_class_id, '未知')
+                object_confidence = conf_res[i]
+                violations_list.append(object_class_name)
 
                 # 添加检测信息
                 detection_info = {
-                    'id': bbox_id,
-                    'class_name': object_class_name,
-                    'bbox': [x1, y1, x2, y2],
+                    # 'id': bbox_id,
+                    # 'class_name': object_class_name,
+                    # 'bbox': [x1, y1, x2, y2],
+                    "score": object_confidence,
+                    "leftTopX": x1,
+                    "leftTopY": y1,
+                    "rightBottomX": x2,
+                    "rightBottomY": y2
                 }
                 detections.append(detection_info)
                 # 在图像上画出边界框
@@ -72,15 +112,14 @@ def upload():
                 # 使用 PIL 在边界框旁边添加中文文本
                 pil_image = Image.fromarray(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
                 draw = ImageDraw.Draw(pil_image)
-                font = ImageFont.truetype("/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc", 40)
+                font = ImageFont.truetype(get_font(), 40)
                 text = f'ID: {bbox_id} 类别: {object_class_name}'
                 draw.text((x1, y1 - 25), text, color[::-1], font=font)
                 cv_image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
 
                 bbox_id += 1  # 递增 bbox ID
 
-
-
+        cv2.imwrite('cv_image.jpg', cv_image)
         # 将 OpenCV 图像转换回 PIL 图像
         pil_image = Image.fromarray(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
 
@@ -92,10 +131,33 @@ def upload():
         end_time = time.time()
         inference_time = end_time - start_time
 
+        tz = pytz.timezone('Asia/Shanghai')
+        beijing_time = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+
+        # return jsonify({
+        #     'inference_time': inference_time,
+        #     'marked_image_base64': img_str,
+        #     'violations': detections
+        # })
         return jsonify({
-            'inference_time': inference_time,
-            'marked_image_base64': img_str,
-            'violations': detections
+            'resultCode': 200,
+            'resultValue': {
+                'analyseResults': "速插或安全带使用不规范" if any(item in violations_list for item in
+                                                                  ['速插位置系错', '速插松弛', '速插低挂高用',
+                                                                   '速插未系', '安全带未系', '安全带低挂高用']
+                                                                  ) else "正常",
+                'analyseTime': beijing_time,
+                'rawImageData': image_data,
+                'osdImageData': img_str,
+                'resultDetail':
+                    {
+                        'algCode': '81001001',
+                        'resultDesc': violations_list,
+                        'num': len(detections),
+                        'resultItems': detections
+                    }
+            },
+            "resultHint": None
         })
 
 
